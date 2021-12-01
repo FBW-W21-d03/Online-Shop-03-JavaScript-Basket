fc83bca50a208af3bc8a3f9196c11a113b817cfd
sprotte <metatron1972@live.de>
2021-12-01 14:52:28 +0100

    sprotte

---
+++ index.js
@@ -0,0 +1,57 @@
+
+/*// FUNKTION: RECHNET EURO IN DOLLAR
+function euroInDollar(){
+    // WAS HAT DER NUTZER EINGEGEBEN?
+    let euro = document.getElementById("euro").value;
+    // UMRECHNUNG IN DOLLAR
+    let dollar = euro * 1.14;
+    // EINSETZEN INS DOLLAR FELD
+    document.getElementById("Dollar").value = dollar;
+}
+
+function dollarInEuro(){
+    let dollar = document.getElementById("Dollar").value;
+    let euro = dollar * 0.86;
+    document.getElementById("euro").value = euro;}
+ */   
+function umrechnen(){
+    document.getElementById("knopf").innerHTML= 320.00 * 1.14;
+}
+
+function umrechnen1(){
+    document.getElementById("knopf1").innerHTML= 49.95 * 1.14;
+}
+
+function umrechnen2(){
+    document.getElementById("knopf2").innerHTML= 860.00 * 1.14;
+}
+function umrechnen3(){
+    document.getElementById("knopf3").innerHTML= 548.00 * 1.14;
+}
+function umrechnen4(){
+    document.getElementById("knopf4").innerHTML= 98.00 * 1.14;
+}
+function umrechnen5(){
+    document.getElementById("knopf5").innerHTML= 428.00 * 1.14;
+}
+function umrechnen6(){
+    document.getElementById("knopf6").innerHTML= 179.00 * 1.14;
+}
+function umrechnen7(){
+    document.getElementById("knopf7").innerHTML= 299.00 * 1.14;
+}
+function umrechnen8(){
+    document.getElementById("knopf8").innerHTML= 57.00 * 1.14;
+}
+function umrechnen9(){
+    document.getElementById("knopf9").innerHTML= 589.00 * 1.14;
+}
+function umrechnen10(){
+    document.getElementById("knopf10").innerHTML= 299.00 * 1.14;
+}
+function umrechnen11(){
+    document.getElementById("knopf11").innerHTML= 989.00 * 1.14;
+}
+
+
+
