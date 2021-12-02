6ce29b2cd6e8440070d61adf6587e9dede41a524
jaber-1988 <jaber1988.1988@gmail.com>
2021-12-02 10:57:15 +0100

    jaber

---
+++ main.js
@@ -0,0 +1,108 @@
+// 1
+function kaufenLabtopHp() {
+    document.getElementById("list").innerHTML =
+    document.getElementById("list").innerHTML + "<li>" + "650 €" + "</li>";
+  
+            // Gesamtsumme
+    document.getElementById("gesamtbetrag").innerHTML =
+                // gesamtbetrag + neues Produkt
+    Number(document.getElementById("gesamtbetrag").innerHTML)
+    +
+    Number(document.getElementById("preisLabtopHp").innerHTML);
+  }
+//   2
+function kaufenHeadset() {
+    document.getElementById("list").innerHTML =
+    document.getElementById("list").innerHTML + "<li>" + "50 €" + "</li>";
+  
+            // Gesamtsumme
+    document.getElementById("gesamtbetrag").innerHTML =
+                // gesamtbetrag + neues Produkt
+    Number(document.getElementById("gesamtbetrag").innerHTML)
+    +
+    Number(document.getElementById("preisHeadset").innerHTML);
+  }
+// 3
+  function kaufenLabtopLinovo() {
+    document.getElementById("list").innerHTML =
+    document.getElementById("list").innerHTML + "<li>" + "750 €" + "</li>";
+  
+            // Gesamtsumme
+    document.getElementById("gesamtbetrag").innerHTML =
+                // gesamtbetrag + neues Produkt
+    Number(document.getElementById("gesamtbetrag").innerHTML)
+    +
+    Number(document.getElementById("preisLabtopLinovo").innerHTML);
+  }
+// 4
+  function kaufenTablet() {
+    document.getElementById("list").innerHTML =
+    document.getElementById("list").innerHTML + "<li>" + "350 €" + "</li>";
+  
+            // Gesamtsumme
+    document.getElementById("gesamtbetrag").innerHTML =
+                // gesamtbetrag + neues Produkt
+    Number(document.getElementById("gesamtbetrag").innerHTML)
+    +
+    Number(document.getElementById("preisTablet").innerHTML);
+  }
+// 5
+  function kaufenHeadsetSony() {
+    document.getElementById("list").innerHTML =
+    document.getElementById("list").innerHTML + "<li>" + "150 €" + "</li>";
+  
+            // Gesamtsumme
+    document.getElementById("gesamtbetrag").innerHTML =
+                // gesamtbetrag + neues Produkt
+    Number(document.getElementById("gesamtbetrag").innerHTML)
+    +
+    Number(document.getElementById("preisHeadsetSony").innerHTML);
+  }
+// 6
+  function kaufenLabtopmsi() {
+    document.getElementById("list").innerHTML =
+    document.getElementById("list").innerHTML + "<li>" + "600 €" + "</li>";
+  
+            // Gesamtsumme
+    document.getElementById("gesamtbetrag").innerHTML =
+                // gesamtbetrag + neues Produkt
+    Number(document.getElementById("gesamtbetrag").innerHTML)
+    +
+    Number(document.getElementById("preisLabtopmsi").innerHTML);
+  }
+// 7
+  function kaufenSmartphone() {
+    document.getElementById("list").innerHTML =
+    document.getElementById("list").innerHTML + "<li>" + "450 €" + "</li>";
+  
+            // Gesamtsumme
+    document.getElementById("gesamtbetrag").innerHTML =
+                // gesamtbetrag + neues Produkt
+    Number(document.getElementById("gesamtbetrag").innerHTML)
+    +
+    Number(document.getElementById("preisSmartphone").innerHTML);
+  }
+// 8
+function kaufenLabtopAsus() {
+  document.getElementById("list").innerHTML =
+  document.getElementById("list").innerHTML + "<li>" + "850 €" + "</li>";
+
+          // Gesamtsumme
+  document.getElementById("gesamtbetrag").innerHTML =
+              // gesamtbetrag + neues Produkt
+  Number(document.getElementById("gesamtbetrag").innerHTML)
+  +
+  Number(document.getElementById("preisLabtopAsus").innerHTML);
+}
+// 9
+function kaufenCamera() {
+  document.getElementById("list").innerHTML =
+  document.getElementById("list").innerHTML + "<li>" + "499 €" + "</li>";
+
+          // Gesamtsumme
+  document.getElementById("gesamtbetrag").innerHTML =
+              // gesamtbetrag + neues Produkt
+  Number(document.getElementById("gesamtbetrag").innerHTML)
+  +
+  Number(document.getElementById("preisCamera").innerHTML);
+}
