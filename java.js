b90c877172444b1261462b13df9437d1aeeda8fd
Serdar68 <Serdarbalkis@outlook.de>
2021-12-02 11:13:23 +0100

    fertig list

---
+++ java.js
@@ -0,0 +1,234 @@
+let euro = false
+
+
+function myFunction(){
+    //document = greift aus html seite zu
+    //getElementById = greift auf die benannte Id zu
+    
+    console.log("es funkioniert")
+    console.log(euro)
+    euro = !euro
+
+    if (euro) { document.getElementById("button1").innerHTML = ("In €")
+        
+    } else { document.getElementById("button1").innerHTML = ("In $")
+    }
+     
+    if (euro) { document.getElementById("wert").innerHTML = ("$")}
+    
+    else { document.getElementById("wert").innerHTML = ("€")
+    }
+
+    if (euro) { document.getElementById("wert1").innerHTML = ("$")}
+    
+    else { document.getElementById("wert1").innerHTML = ("€")
+    }
+
+    if (euro) { document.getElementById("wert2").innerHTML = ("$")}
+    
+    else { document.getElementById("wert2").innerHTML = ("€")
+    }
+
+    if (euro) { document.getElementById("wert3").innerHTML = ("$")}
+    
+    else { document.getElementById("wert3").innerHTML = ("€")
+    }
+
+    if (euro) { document.getElementById("wert4").innerHTML = ("$")}
+    
+    else { document.getElementById("wert4").innerHTML = ("€")
+    }
+
+    if (euro) { document.getElementById("wert5").innerHTML = ("$")}
+    
+    else { document.getElementById("wert5").innerHTML = ("€")
+    }
+
+    if (euro) { document.getElementById("wert6").innerHTML = ("$")}
+    
+    else { document.getElementById("wert6").innerHTML = ("€")
+    }
+
+    if (euro) { document.getElementById("wert7").innerHTML = ("$")}
+    
+    else { document.getElementById("wert7").innerHTML = ("€")
+    }
+
+    if (euro) { document.getElementById("wert8").innerHTML = ("$")}
+    
+    else { document.getElementById("wert8").innerHTML = ("€")
+    }
+     
+    if (euro){ document.getElementById("card-link").innerHTML = (document.getElementById("card-link").innerHTML * 1.14).toFixed(2)  ;
+             }
+    else { document.getElementById("card-link").innerHTML = (document.getElementById("card-link").innerHTML / 1.14).toFixed(2);
+         }
+
+         if (euro){ document.getElementById("card-link1").innerHTML = (document.getElementById("card-link1").innerHTML * 1.14).toFixed(2)  ;
+             }
+    else { document.getElementById("card-link1").innerHTML = (document.getElementById("card-link1").innerHTML / 1.14).toFixed(2);
+         }
+
+         if (euro){ document.getElementById("card-link2").innerHTML = (document.getElementById("card-link2").innerHTML * 1.14).toFixed(2)  ;
+             }
+    else { document.getElementById("card-link2").innerHTML = (document.getElementById("card-link2").innerHTML / 1.14).toFixed(2);
+         }
+
+         if (euro){ document.getElementById("card-link3").innerHTML = (document.getElementById("card-link3").innerHTML * 1.14).toFixed(2)  ;
+             }
+    else { document.getElementById("card-link3").innerHTML = (document.getElementById("card-link3").innerHTML / 1.14).toFixed(2);
+         }
+
+         if (euro){ document.getElementById("card-link4").innerHTML = (document.getElementById("card-link4").innerHTML * 1.14).toFixed(2)  ;
+             }
+    else { document.getElementById("card-link4").innerHTML = (document.getElementById("card-link4").innerHTML / 1.14).toFixed(2);
+         }
+
+         if (euro){ document.getElementById("card-link5").innerHTML = (document.getElementById("card-link5").innerHTML * 1.14).toFixed(2)  ;
+             }
+    else { document.getElementById("card-link5").innerHTML = (document.getElementById("card-link5").innerHTML / 1.14).toFixed(2);
+         }
+
+         if (euro){ document.getElementById("card-link6").innerHTML = (document.getElementById("card-link6").innerHTML * 1.14).toFixed(2)  ;
+             }
+    else { document.getElementById("card-link6").innerHTML = (document.getElementById("card-link6").innerHTML / 1.14).toFixed(2);
+         }
+
+         if (euro){ document.getElementById("card-link7").innerHTML = (document.getElementById("card-link7").innerHTML * 1.14).toFixed(2)  ;
+             }
+    else { document.getElementById("card-link7").innerHTML = (document.getElementById("card-link7").innerHTML / 1.14).toFixed(2);
+         }
+         
+         if (euro){ document.getElementById("card-link8").innerHTML = (document.getElementById("card-link8").innerHTML * 1.14).toFixed(2)  ;
+        }
+    else { document.getElementById("card-link8").innerHTML = (document.getElementById("card-link8").innerHTML / 1.14).toFixed(2);
+    }
+}
+
+
+
+function nootbook(){
+    document.getElementById("list").innerHTML =  
+    document.getElementById("list").innerHTML
+     + 
+     "<li>" + "ASUS Vivobook S17 - 499.00 €" + "</li>";
+
+    document.getElementById("summe").innerHTML = 
+    Number(document.getElementById("summe").innerHTML)
+     + 
+    Number(document.getElementById("card-link").innerHTML)
+
+    console.log("esgeht")
+}
+
+function headphon(){
+    document.getElementById("list").innerHTML =  
+    document.getElementById("list").innerHTML
+     + 
+     "<li>" + "SENNHEISER HD 450BT Over-ear - 299.00 €" + "</li>";
+
+    document.getElementById("summe").innerHTML = 
+    Number(document.getElementById("summe").innerHTML)
+     + 
+    Number(document.getElementById("card-link1").innerHTML)
+
+    console.log("esgeht1")
+}
+
+function nootbook1(){
+    document.getElementById("list").innerHTML =  
+    document.getElementById("list").innerHTML
+     + 
+     "<li>" + "SAMSUNG GALAXY BOOK PRO 360 EVO - 289.00 €" + "</li>";
+
+    document.getElementById("summe").innerHTML = 
+    Number(document.getElementById("summe").innerHTML)
+     + 
+    Number(document.getElementById("card-link2").innerHTML)
+
+    console.log("esgeht2")
+}
+
+function ipad1(){
+    document.getElementById("list").innerHTML =  
+    document.getElementById("list").innerHTML
+     + 
+     "<li>" + "Sony TEB-15DSKP Tablet - 173.00 €" + "</li>";
+
+    document.getElementById("summe").innerHTML = 
+    Number(document.getElementById("summe").innerHTML)
+     + 
+    Number(document.getElementById("card-link3").innerHTML)
+
+    console.log("esgeht3")
+}
+
+function headphone1(){
+    document.getElementById("list").innerHTML =  
+    document.getElementById("list").innerHTML
+     + 
+     "<li>" + "SONY WH-1000XM3 Noise Cancelling - 530.00 €" + "</li>";
+
+    document.getElementById("summe").innerHTML = 
+    Number(document.getElementById("summe").innerHTML)
+     + 
+    Number(document.getElementById("card-link4").innerHTML)
+
+    console.log("esgeht4")
+}
+
+function nootbook2(){
+    document.getElementById("list").innerHTML =  
+    document.getElementById("list").innerHTML
+     + 
+     "<li>" + "MSI KATANA (GF76) Gaming Notebook - 999.00 €" + "</li>";
+
+    document.getElementById("summe").innerHTML = 
+    Number(document.getElementById("summe").innerHTML)
+     + 
+    Number(document.getElementById("card-link5").innerHTML)
+
+    console.log("esgeht5")
+}
+
+function phone(){
+    document.getElementById("list").innerHTML =  
+    document.getElementById("list").innerHTML
+     + 
+     "<li>" + "SAMSUNG Galaxy S7 32 GB Black-Onyx - 329.00 €" + "</li>";
+
+    document.getElementById("summe").innerHTML = 
+    Number(document.getElementById("summe").innerHTML)
+     + 
+    Number(document.getElementById("card-link6").innerHTML)
+
+    console.log("esgeht6")
+}
+
+function nootbook3(){
+    document.getElementById("list").innerHTML =  
+    document.getElementById("list").innerHTML
+     + 
+     "<li>" + "ASUS ROG Strix G17 (G713QM-HX019T) - 890.00 €" + "</li>";
+
+    document.getElementById("summe").innerHTML = 
+    Number(document.getElementById("summe").innerHTML)
+     + 
+    Number(document.getElementById("card-link7").innerHTML)
+
+    console.log("esgeht7")
+}
+
+function cam(){
+    document.getElementById("list").innerHTML =  
+    document.getElementById("list").innerHTML
+     + 
+     "<li>" + "Digitalkamera 1080P HD - 325.00 €" + "</li>";
+
+    document.getElementById("summe").innerHTML = 
+    Number(document.getElementById("summe").innerHTML)
+     + 
+    Number(document.getElementById("card-link8").innerHTML)
+
+    console.log("esgeht8")
+}
